d909877f5b30b1a00cd9097bf57bd37121b65581
ShamsEE96 <wolfsh96@gmail.com>
2023-08-08 06:08:33 +0300

    Added const to all Colors

--- lib/ui/shared/colors.dart
+++ lib/ui/shared/colors.dart
@@ -1,35 +1,36 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color whiteColor = Color.fromRGBO(255, 255, 255, 1);
-  static Color blackColor = Color.fromRGBO(0, 0, 0, 1);
-  static Color mainOrangeColor = Color.fromRGBO(252, 96, 17, 1);
-  static Color transparentColor = Color.fromRGBO(255, 255, 255, 0);
-  static Color maingrey = Color.fromRGBO(242, 242, 242, 1);
-  static Color maaingreeydark = Color.fromRGBO(182, 183, 183, 1);
-  static Color mainblack = Color.fromRGBO(74, 75, 77, 1);
-  static Color mainlightgrey = Color.fromRGBO(124, 125, 126, 1);
-  static Color mainbluecolor = Color.fromRGBO(54, 127, 192, 1);
-  static Color mainRedcolr = Color.fromRGBO(221, 75, 57, 1);
-  static Color mainback = Color.fromRGBO(242, 242, 242, 1);
-  static Color mainGrey = Color.fromRGBO(214, 214, 214, 1);
-  // static Color mainOrangeColor = Color.fromRGBO(252, 96, 17, 1);
-  static Color mainBlueColor = Color.fromRGBO(54, 127, 192, 1);
-  static Color mainRedColor = Color.fromRGBO(221, 75, 57, 1);
-  // static Color mainWhiteColor = Color.fromRGBO(255, 255, 255, 1);
-  static Color mainGreenColor = Color.fromRGBO(45, 221, 10, 1);
-  static Color transparentWhiteColor = Color.fromRGBO(255, 255, 255, 0.23);
-  // static Color transparentColor = Color.fromRGBO(255, 255, 255, 0);
-  static Color mainGreyColor = Color.fromRGBO(74, 75, 77, 1);
-  static Color secondaryGreyColor = Color.fromRGBO(124, 125, 126, 1);
-  static Color placeholderGreyColor = Color.fromRGBO(182, 183, 183, 1);
-  static Color fillGreyColor = Color.fromRGBO(242, 242, 242, 1);
+  static const Color whiteColor = Color.fromRGBO(255, 255, 255, 1);
+  static const Color blackColor = Color.fromRGBO(0, 0, 0, 1);
+  static const Color mainOrangeColor = Color.fromRGBO(252, 96, 17, 1);
+  static const Color transparentColor = Color.fromRGBO(255, 255, 255, 0);
+  static const Color maingrey = Color.fromRGBO(242, 242, 242, 1);
+  static const Color maaingreeydark = Color.fromRGBO(182, 183, 183, 1);
+  static const Color mainblack = Color.fromRGBO(74, 75, 77, 1);
+  static const Color mainlightgrey = Color.fromRGBO(124, 125, 126, 1);
+  static const Color mainbluecolor = Color.fromRGBO(54, 127, 192, 1);
+  static const Color mainRedcolr = Color.fromRGBO(221, 75, 57, 1);
+  static const Color mainback = Color.fromRGBO(242, 242, 242, 1);
+  static const Color mainGrey = Color.fromRGBO(214, 214, 214, 1);
+  // static const  Color mainOrangeColor = Color.fromRGBO(252, 96, 17, 1);
+  static const Color mainBlueColor = Color.fromRGBO(54, 127, 192, 1);
+  static const Color mainRedColor = Color.fromRGBO(221, 75, 57, 1);
+  // static const  Color mainWhiteColor = Color.fromRGBO(255, 255, 255, 1);
+  static const Color mainGreenColor = Color.fromRGBO(45, 221, 10, 1);
+  static const Color transparentWhiteColor =
+      Color.fromRGBO(255, 255, 255, 0.23);
+  // static const  Color transparentColor = Color.fromRGBO(255, 255, 255, 0);
+  static const Color mainGreyColor = Color.fromRGBO(74, 75, 77, 1);
+  static const Color secondaryGreyColor = Color.fromRGBO(124, 125, 126, 1);
+  static const Color placeholderGreyColor = Color.fromRGBO(182, 183, 183, 1);
+  static const Color fillGreyColor = Color.fromRGBO(242, 242, 242, 1);
 
   //!---- App Colors from figma ----
-  static Color darkPurpleColor = Color.fromRGBO(111, 40, 119, 1);
-  static Color normalPurpleColor = Color.fromRGBO(146, 63, 152, 1);
-  static Color lightPurpleColor = Color.fromRGBO(185, 137, 189, 1);
-  static Color normalCyanColor = Color.fromRGBO(32, 196, 244, 1);
-  static Color lightCyanColor = Color.fromRGBO(185, 229, 251, 1);
-  static Color darkGreyColor = Color.fromRGBO(70, 69, 71, 1);
+  static const Color darkPurpleColor = Color.fromRGBO(111, 40, 119, 1);
+  static const Color normalPurpleColor = Color.fromRGBO(146, 63, 152, 1);
+  static const Color lightPurpleColor = Color.fromRGBO(185, 137, 189, 1);
+  static const Color normalCyanColor = Color.fromRGBO(32, 196, 244, 1);
+  static const Color lightCyanColor = Color.fromRGBO(185, 229, 251, 1);
+  static const Color darkGreyColor = Color.fromRGBO(70, 69, 71, 1);
 }

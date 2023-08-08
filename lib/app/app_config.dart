918d7ca72b24ebb39fdd805553048c1aee31c502
ShamsEE96 <wolfsh96@gmail.com>
2023-08-08 05:46:03 +0300

    Changed default language to ar

--- lib/app/app_config.dart
+++ lib/app/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
   static bool isLocationRequired = true;
-  static String defaultLanguage = 'en';
+  static String defaultLanguage = 'ar';
   static bool scriptionStatus = true;
 }

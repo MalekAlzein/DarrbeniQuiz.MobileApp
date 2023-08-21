7f97f338e2c09bfb7be510e4a84a4c9b56318180
ShamsEE96 <abdulrahman.shamse@gmail.com>
2023-08-21 16:21:54 +0300

    Added the slider api call to home_view

--- lib/ui/views/main_view/home_view/home_controller.dart
+++ lib/ui/views/main_view/home_view/home_controller.dart
@@ -1,3 +1,36 @@
+import 'package:flutter_templete/core/data/models/apis/slider_model.dart';
+import 'package:flutter_templete/core/data/reposotories/silder_repository.dart';
+import 'package:flutter_templete/core/enums/message_type.dart';
 import 'package:flutter_templete/core/services/base_controller.dart';
+import 'package:flutter_templete/ui/shared/custom_widgets/custom_toast.dart';
+import 'package:get/get.dart';
 
-class HomeController extends BaseController {}
+class HomeController extends BaseController {
+  RxList<SilderModel> silderList = <SilderModel>[].obs;
+  @override
+  void onInit() {
+    getAllSliders();
+    super.onInit();
+  }
+
+  void getAllSliders() {
+    runFutureFunction(
+      function: SliderRepository().getAllSliders().then(
+        (value) {
+          value.fold((l) {
+            CustomToast.showMessage(
+              messageType: MessageType.REJECTED,
+              message: l,
+            );
+          }, (r) {
+            silderList.addAll(r);
+            CustomToast.showMessage(
+              message: "Success",
+              messageType: MessageType.SUCCESS,
+            );
+          });
+        },
+      ),
+    );
+  }
+}

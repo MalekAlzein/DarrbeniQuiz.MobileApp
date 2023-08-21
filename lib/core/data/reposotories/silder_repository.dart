7f671e39bb1fa787c54b9945b9eca2f3f094b702
ShamsEE96 <abdulrahman.shamse@gmail.com>
2023-08-21 16:21:18 +0300

    Added slider api model, repo , endpoints

---
+++ lib/core/data/reposotories/silder_repository.dart
@@ -0,0 +1,48 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_templete/core/data/models/apis/college_model.dart';
+import 'package:flutter_templete/core/data/models/apis/slider_model.dart';
+import 'package:flutter_templete/core/data/network/endpoints/college_endpoints.dart';
+import 'package:flutter_templete/core/data/network/endpoints/slider_endpoints.dart';
+
+import '../../enums/request_type.dart';
+import '../../utils/network_utils.dart';
+import '../models/common_response.dart';
+import '../network/network_config.dart';
+
+class SliderRepository {
+  Future<Either<String, List<SilderModel>>> getAllSliders() async {
+    try {
+      return NetworkUtil.sendRequest(
+        requestType: RequestType.GET,
+        url: SliderEndpoints.allSliders,
+        headers: NetworkConfig.getHeaders(
+          needAuth: false,
+          requestType: RequestType.GET,
+        ),
+        body: {},
+        params: {},
+      ).then(
+        (response) {
+          CommonResponseModel<Map<String, dynamic>> commonResponse =
+              CommonResponseModel.fromJson(response);
+
+          if (commonResponse.getStatus) {
+            List<SilderModel> resultList = [];
+
+            commonResponse.data!["sliders"].forEach(
+              (element) {
+                resultList.add(SilderModel.fromJson(element));
+              },
+            );
+
+            return Right(resultList);
+          } else {
+            return Left(commonResponse.message ?? "");
+          }
+        },
+      );
+    } catch (e) {
+      return Left(e.toString());
+    }
+  }
+}

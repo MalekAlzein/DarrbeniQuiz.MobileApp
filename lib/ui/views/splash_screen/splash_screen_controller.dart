c6a14d7910fb73333eb8903dec10791e21fa5d97
ShamsEE96 <abdulrahman.shamse@gmail.com>
2023-08-23 16:14:44 +0300

    Fixed Avatar Added BottomSheet for image picker

--- lib/ui/views/splash_screen/splash_screen_controller.dart
+++ lib/ui/views/splash_screen/splash_screen_controller.dart
@@ -3,8 +3,6 @@ import 'package:flutter_templete/core/enums/message_type.dart';
 import 'package:flutter_templete/core/utils/general_utils.dart';
 import 'package:flutter_templete/ui/shared/custom_widgets/custom_toast.dart';
 import 'package:flutter_templete/ui/views/login_view/login_view.dart';
-import 'package:flutter_templete/ui/views/main_view/main_view.dart';
-import 'package:flutter_templete/ui/views/signup_view/signup_view.dart';
 import 'package:get/get.dart';
 
 class SplashScreenController extends GetxController {

16c8d0888a08795728b7f9e01aac5ae406baa7dd
QamarBrejawe <qamarbrejawe23@gmail.com>
2023-09-02 14:55:21 +0300

    update models2

--- lib/core/data/network/endpoints/specialization_endpoints.dart
+++ lib/core/data/network/endpoints/specialization_endpoints.dart
@@ -6,10 +6,10 @@ class SpecializationEndpoints {
   static String allCollages = NetworkConfig.getFullApiRoute('Collages/all');
   static String allSpecialization =
       NetworkConfig.getFullApiRoute('Specialization/allSpecialization');
-  static String moreOptions =
-      NetworkConfig.getFullApiRoute('Specialization/checkButtons');
   static String ShowMaster =
       NetworkConfig.getFullApiRoute('Specialization/filters/type/master');
   static String ShowGraduation =
       NetworkConfig.getFullApiRoute('Specialization/filters/type/graduation');
+  static String ShowSubject =
+      NetworkConfig.getFullApiRoute('Specialization/filters/specialization/');
 }

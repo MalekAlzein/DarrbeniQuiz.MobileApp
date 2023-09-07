b851d2c67f35f0e2089beaa4aae00338ba132986
QamarBrejawe <qamarbrejawe23@gmail.com>
2023-09-07 22:10:57 +0300

    qit pull

--- lib/core/enums/operation_type.dart
+++ lib/core/enums/operation_type.dart
@@ -1,8 +1,1 @@
-<<<<<<< HEAD
-enum OperationType { NONE, QUESTION }
-=======
-enum OperationType {
-  NONE,
-  HOME,
-}
->>>>>>> 1cd9f9754b01608acbe74b8d8524a561b141b1b4
+enum OperationType { NONE, HOME, QUESTION }

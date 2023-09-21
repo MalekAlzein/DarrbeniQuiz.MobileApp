6577176ad418afb8f5aa46d20c53e13dc07fe299
HOMAM <homamr06@gmail.com>
2023-09-21 18:53:46 +0300

    update notifaction1

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -8,7 +8,6 @@ import 'package:flutter_templete/ui/shared/colors.dart';
 import 'package:flutter_templete/ui/views/main_view/main_view.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
-import '../ui/views/main_view/notifications_view/notifications_view.dart';
 import '../ui/views/splash_screen/spalsh_screen_view.dart';
 
 late BuildContext appContext;
@@ -57,7 +56,7 @@ class _MyAppState extends State<MyApp> {
             secondary: Colors.transparent.withOpacity(0),
           ),
         ),
-        home: NotificationsView(),
+        home: SplashScreenView(),
       ),
     );
     //
